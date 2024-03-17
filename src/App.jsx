852ca6c70eda48188d73e7c9799ef377ed53fa32
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2024-03-17 13:32:31 +0700

    add theme (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,60 @@
+//mui
 import Button from '@mui/material/Button'
 import AccessAlarm from '@mui/icons-material/AccessAlarm'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <Box sx={{ display:'flex', alignItems: 'center', gap:'8px' }}><LightModeIcon fontSize='small'/> Light</Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display:'flex', alignItems: 'center', gap:'8px' }}><DarkModeOutlinedIcon fontSize='small'/> Dark</Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display:'flex', alignItems: 'center', gap:'8px' }}><SettingsBrightnessIcon fontSize='small'/> System</Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
 
 function App() {
   return (
     <>
-      <div>trung quan dev</div>
+      <ModeSelect/>
+      <hr/>
+      <div>anh quoc dev</div>
+      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
       <Button variant="text">TEXT</Button>
       <Button variant="contained">CONTAINED</Button>
       <Button variant="outlined">OUTLINE</Button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import CssBaseline from '@mui/material/CssBaseline';
+
+//mui
+import CssBaseline from '@mui/material/CssBaseline'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+
+//theme
+import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline/>
-    <App />
+    <CssVarsProvider theme={theme}>
+      <CssBaseline/>
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>
 )
