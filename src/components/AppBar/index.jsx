d6560e30f8fe2ad8953c7d8f07cf35f217379606
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2024-03-17 22:14:06 +0700

    refactor_project_structure (#7)

---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,23 @@
+//mui
+import Box from "@mui/material/Box";
+
+//component
+import ModeSelect from "../../components/ModeSelect";
+
+function AppBar() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "primary.light",
+        width: "100%",
+        height: (theme) => theme.trello.appBarHeight,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <ModeSelect />
+    </Box>
+  );
+}
+
+export default AppBar;
---
+++ src/pages/Boards/BoardBar/index.jsx
@@ -0,0 +1,20 @@
+//mui
+import Box from "@mui/material/Box";
+
+function BoardBar() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "primary.dark",
+        width: "100%",
+        height: (theme) => theme.trello.boardBarHeight,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      BoardBar
+    </Box>
+  );
+}
+
+export default BoardBar;
