b7556f1bfbbe35a4c578513407d8c9bb6bb61615
QuocAnh189 <anhquoc18092003@gmail.com>
2024-03-18 00:24:54 +0700

    add ui header

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,21 +1,96 @@
 //mui
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
+import Badge from "@mui/material/Badge";
 
 //component
 import ModeSelect from "~/components/ModeSelect";
+import Workspaces from "./Menus/Workspaces";
+import Recent from "./Menus/Recent";
+import Template from "./Menus/Template";
+import Profile from "./Menus/Profile";
+
+//icon
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import { ReactComponent as TrelloIcon } from "~/assets/trello.svg";
+import SvgIcon from "@mui/material/SvgIcon";
+import Starred from "./Menus/Starred";
 
 function AppBar() {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: "primary.light",
         width: "100%",
         height: (theme) => theme.trello.appBarHeight,
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between",
+        gap: 2,
+        overflowX: "auto",
       }}
     >
-      <ModeSelect />
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <AppsIcon sx={{ color: "primary.main" }} />
+        <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+          <SvgIcon
+            component={TrelloIcon}
+            fontSize="small"
+            inheritViewBox
+            sx={{ color: "primary.main" }}
+          />
+          <Typography
+            variant="span"
+            sx={{
+              fontSize: "1.2rem",
+              fontWeight: "bold",
+              color: "primary.main",
+            }}
+          >
+            Trello
+          </Typography>
+        </Box>
+        <Box sx={{ display: { xs: "none", md: "flex" }, gap: 1 }}>
+          <Workspaces />
+          <Recent />
+          <Starred />
+          <Template />
+          <Button
+            variant="outlined"
+            sx={{
+              fontWeight: "bold",
+            }}
+          >
+            Create
+          </Button>
+        </Box>
+      </Box>
+
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <TextField
+          id="outlined-search"
+          label="Search..."
+          type="search"
+          size="small"
+          sx={{ minWidth: "120px" }}
+        />
+        <ModeSelect />
+        <Tooltip title="Notification">
+          <Badge color="secondary" variant="dot" sx={{ cursor: "pointer" }}>
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+
+        <Tooltip title="Help" sx={{ cursor: "pointer" }}>
+          <HelpOutlineIcon />
+        </Tooltip>
+        <Profile />
+      </Box>
     </Box>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,79 @@
-import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { teal, deepOrange, cyan, orange } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = extendTheme({
-  trello:{
-    appBarHeight: '48px',
-    boardBarHeight: '58px'
+  trello: {
+    appBarHeight: "58px",
+    boardBarHeight: "60px",
   },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
-        secondary: deepOrange
-      }
+        secondary: deepOrange,
+      },
     },
     dark: {
       palette: {
         primary: cyan,
-        secondary: orange
-      }
-    }
-  }
-})
+        secondary: orange,
+      },
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          "*::-webkit-scrollbar": {
+            width: "8px",
+            height: "8px",
+          },
+          "*::-webkit-scrollbar-thumb": {
+            backgroundColor: "#bdc3c7",
+            borderRadius: "8px",
+          },
+          "*::-webkit-scrollbar-thumb:hover": {
+            backgroundColor: "#00b894",
+            borderRadius: "8px",
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({ color: theme.palette.primary.main }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          return {
+            color: theme.palette.primary.main,
+            fontSize: "0.875rem",
+            ".MuiOutlinedInput-notchedOutline": {
+              borderColor: theme.palette.primary.light,
+            },
+            "&:hover": {
+              ".MuiOutlinedInput-notchedOutline": {
+                borderColor: theme.palette.primary.light,
+              },
+            },
+            "& fieldset": {
+              borderWidth: "1px !important",
+            },
+          };
+        },
+      },
+    },
+  },
+});
 
-export default theme
+export default theme;
