9b3f321392a498aaf7aa958b319030dfed36b924
Trần Phước Anh Quốc <anhquoc18092003@gmail.com>
2024-03-17 22:42:00 +0700

    config import file (#8)

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,7 +2,7 @@
 import Container from "@mui/material/Container";
 
 //component
-import AppBar from "../../components/AppBar";
+import AppBar from "~/components/AppBar";
 import BoardBar from "./BoardBar";
 import BoardContent from "./BoardContent";
 
